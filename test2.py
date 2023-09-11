c9d9f6c622c5d9e79e6f896c4ef54f89497a4770
isaacsue <805890851@qq.com>
2023-09-11 23:56:31 +0800

    main logic

--- test2.py
+++ test2.py
@@ -1,6 +1,4 @@
-
-
-#   stage 1 
+#   stage 1
 #  initiation
 #  distrubte sk and pk
 
@@ -8,7 +6,7 @@ import numpy as np
 from Crypto.Random import random
 
 
-def key_gen() :
+def key_gen():
     return 0
 
 
@@ -17,100 +15,86 @@ class player:
     def __init__(self, sk):
         self._sk = sk
 
+
 class deck:
-    def __init__(self, num_cards):  
+    def __init__(self, num_cards):
         self._cards = []
         for i in range(num_cards):
             c = card(str(i))
-            c.create_an_open_card(0) ## TODO , change 0 to pk
+            c.create_an_open_card(0)  ## TODO , change 0 to pk
             self._cards.append(c)
 
         self._cards_order = np.array([i for i in range(num_cards)])
 
     def mask_shuffling(self, permutation_matrix, pk, r):
-        mask_zkp =  []
+        mask_zkp = []
         for card in self._cards:
-            _, zkp  =  card.mask_a_card(pk, r)
+            _, zkp = card.mask_a_card(pk, r)
             mask_zkp.append(zkp)
-
 
         self._cards_order = permutation_matrix.dot(self._cards_order)
         # write permutation and generate zkp with circom
         zkp = 321
         return mask_zkp, zkp
 
-
-    def mask_spliting(self,x, pk, r):
+    def mask_splitting(self, x, pk, r):
         mask_zkp = []
         for card in self._cards:
-            _, zkp = card.mask_a_card(pk,r)
+            _, zkp = card.mask_a_card(pk, r)
             mask_zkp.append(zkp)
 
-        self._cards_order = np.roll(self._cards_order, len(self._cards_order)- x)
-        
+        self._cards_order = np.roll(self._cards_order, len(self._cards_order) - x)
 
-
-        
-
-
+    @property
+    def cards(self):
+        return self._cards
 
 
 class card:
     def __init__(self, value):
         self._value = value
         self._encrypted = 0
-    
+
     def encrypt(self, pk, r):
         # TODO implement this encryption
-        self._value = self._value +"#"
-        self._encrypted +=1
-    
+        self._value = self._value + "#"
+        self._encrypted += 1
+
     def decrypt(self, sk, r):
         self._value = self._value[:-1]
 
-    def create_an_open_card(self, pk ):
-        self.encrypt(pk, 1) # open card definition, r = 1
+    def create_an_open_card(self, pk):
+        self.encrypt(pk, 1)  # open card definition, r = 1
 
     def mask_a_card(self, pk, r):
-        self.encrypt(pk, r) #
+        self.encrypt(pk, r)  #
         # TODO 这里需要zkp 
-        
+
         # TODO, generate zkp for this mask op
         zkp = 123
-        
+
         return self._value, zkp
 
     def create_a_private_card(self, pk, r):
         return
-    
 
-    
-
-
-
-
-
+    @property
+    def value(self):
+        return self._value
 
 
 def demo_main():
-
-    #mapping = {}
-    
-
-
+    # mapping = {}
 
     n = input("enter the number of player: ")
     print(int(n))
 
-
-
     players = []
 
     for i in range(int(n)):
-
         # TODO implement k_Gen
         sk = 1 + i
-        pk = sk +2
+        pk = sk + 2
 
         #
         p = player(sk)
@@ -118,18 +102,8 @@ def demo_main():
         print("player's sk is ", p._sk)
         print("player's pk is", pk)
 
-        
-
     num_cards = int(input("input your number of cards used: "))
-    my_deck  = deck(int(num_cards))
-
-
-
-
-    
-
-
-
+    my_deck = deck(int(num_cards))
 
     print("open cards value ciphertext: ")
     for card in my_deck._cards:
@@ -140,29 +114,26 @@ def demo_main():
         # TODO make it more compact
         print(card._value)
 
-    
     card_num = input("input a card index for verification: ")
-    
+
     chosen_card = my_deck._cards[int(card_num)]
     print("open card ciphertext for the chosen card:", chosen_card._value)
 
-    chosen_card.decrypt(0,1)
-    print("open card value plaint text for the chosen card",chosen_card._value)
+    chosen_card.decrypt(0, 1)
+    print("open card value plaint text for the chosen card", chosen_card._value)
 
-    chosen_card.encrypt(0,1)
+    chosen_card.encrypt(0, 1)
 
-    
-    player_idx  = int(input("input your player idx"))
-    card_idx  = int(input("input your card idx"))
+    player_idx = int(input("input your player idx"))
+    card_idx = int(input("input your card idx"))
 
-    p_chosen = players[player_idx]
+    # p_chosen = players[player_idx]
 
     chosen_card = my_deck._cards[card_idx]
 
-    r = 314 #TODO r should be a strong random
+    r = 314  # TODO r should be a strong random
     # write to a json file for circom input
-    
-    
+
     cipher_card, zkp = chosen_card.mask_a_card(pk, r)
 
     print("the masked card is: ")
@@ -170,32 +141,54 @@ def demo_main():
     print("and the corresponding zkp is :")
     print(zkp)
 
-
-## shuffle demo
-    player_idx  = int(input("input your shuffle player"))
+    ## shuffle demo
+    player_idx = int(input("input your shuffle player"))
 
     A = np.identity(num_cards)
     A = np.random.permutation(A)
 
-
-# TODO changge pk and r 
-    my_deck.mask_shuffling(permutation_matrix= A, pk= 0,r = 0)
-    
+    # TODO changge pk and r
+    my_deck.mask_shuffling(permutation_matrix=A, pk=0, r=0)
 
     print(my_deck._cards_order)
 
-# TODO changge pk and r
-    my_deck.mask_spliting(4, pk= 0, r = 0)
+    # TODO changge pk and r
+    my_deck.mask_spliting(4, pk=0, r=0)
     print(my_deck._cards_order)
-    
-
-##
 
 
-
-
-        
-   
-
-
-demo_main()
+if __name__ == '__main__':
+    player_amount = int(input("please input the number of players:"))
+    players = []
+    for i in range(player_amount):
+        players.append(player(key_gen()))
+    card_amount = int(input("please input the number of players:"))
+    my_deck = deck(int(card_amount))
+    print("open cards value ciphertext: ")
+    for card in my_deck.cards:
+        print(card.value)
+    print("open cards ciphertext: ")
+    for card in my_deck._cards:
+        # TODO make it more compact
+        print(card._value)
+    while True:
+        print("Available commands:\n1.encrypt(self, String pk, int r)\n" +
+              "2.decrypt(self, String sk, int r)\n" +
+              "3.mask_a_card(self, String pk, int r)")
+        print("format: number paras")
+        cmd_ipt = input("which command do you want to run? q for quit\n")
+        cmd = cmd_ipt.split()
+        match cmd[0]:
+            case "q":
+                break
+            case "1":
+                index = int(input("input a card index for verification: "))
+                my_deck.cards[index].encrypt(cmd[1], int(cmd[2]))
+            case "2":
+                index = int(input("input a card index for verification: "))
+                my_deck._cards[int(index)].decrypt(cmd[1], int(cmd[2]))
+            case "3":
+                pass
+            case _:
+                print("non-existent command")
+        print()
